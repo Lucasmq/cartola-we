f4daeedac1be43d7163cd774008d511a073931b9
Lucas Queiroz <lucasmedeirosq@gmail.com>
2019-07-09 22:29:40 -0300

    build

---
+++ build/precache-manifest.7ebde7351910924805218491266b43cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ea695c2bd521460c8dbc7394368e72c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8af2b58b0c65a66fe4c0",
+    "url": "/static/css/main.be4fc00f.chunk.css"
+  },
+  {
+    "revision": "0d15dfe63d86f1584721",
+    "url": "/static/js/2.1c6b3cb2.chunk.js"
+  },
+  {
+    "revision": "8af2b58b0c65a66fe4c0",
+    "url": "/static/js/main.b6883d13.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
