61e7bc20795d4ffd3be46b5e33110773573ef943
Lucas Queiroz <lucasmedeirosq@gmail.com>
2019-07-13 16:39:16 -0300

    Updates

---
+++ precache-manifest.2f22340524d1e3c507003b51f395de96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57d58cb2a8387f7c20ccb86cc7c63950",
+    "url": "/cartola-we/index.html"
+  },
+  {
+    "revision": "d614d620be9bf11f354c",
+    "url": "/cartola-we/static/css/main.d989c207.chunk.css"
+  },
+  {
+    "revision": "61e1f330762cd2e071df",
+    "url": "/cartola-we/static/js/2.39d892fe.chunk.js"
+  },
+  {
+    "revision": "d614d620be9bf11f354c",
+    "url": "/cartola-we/static/js/main.81954068.chunk.js"
+  },
+  {
+    "revision": "749c706c2e29d0f107e5",
+    "url": "/cartola-we/static/js/runtime~main.9094853e.js"
+  }
+]);
